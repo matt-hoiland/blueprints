344fbfa1055fec5d1611574087cc8b3e183df9b8
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-07-30 12:23:25 -0600

    ch-2/auth: Route chat endpoint to login

--- cmd/chat-server/main.go
+++ cmd/chat-server/main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/matt-hoiland/blueprints/trace"
 )
 
 var (
-	addr = flag.String("addr", ":8080", "The addr of the application.")
+	host    = flag.String("host", "localhost", "The host of the chat server.")
+	port    = flag.String("port", "8080", "The port of the chat server.")
+	verbose = flag.Bool("v", false, "Enable verbose logging output")
 )
 
 type templateHandler struct {
@@ -21,23 +27,36 @@ type templateHandler struct {
 
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		var err error
+		t.templ, err = template.ParseFiles(filepath.Join("templates", t.filename))
+		if err != nil {
+			panic(err)
+		}
 	})
-	_ = t.templ.Execute(w, r)
+	err := t.templ.Execute(w, r)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 	flag.Parse()
+	addr := fmt.Sprintf("%s:%s", *host, *port)
+	wd, _ := os.Getwd()
+	log.Println("Working Directory", wd)
 
 	r := newRoom()
-	// r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	if *verbose {
+		r.tracer = trace.New(os.Stdout)
+	}
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	// Get the room going
 	go r.run()
 	// Start the web server
-	log.Println("Starting web server on", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	log.Println("Starting web server on", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal("ListenAndServer:", err)
 	}
 }
