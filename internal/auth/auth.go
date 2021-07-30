158652571d16f6f5a0c730c2afd3dce4b526d028
Matt Hoiland <matthew.proctor.hoiland@gmail.com>
2021-07-30 12:44:06 -0600

    ch-2/auth: Break auth decorator into separate file

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -7,28 +7,6 @@ import (
 	"strings"
 )
 
-type authHandler struct {
-	next http.Handler
-}
-
-func MustAuth(handler http.Handler) http.Handler {
-	return &authHandler{next: handler}
-}
-
-func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
-		// not authorized
-		w.Header().Set("Location", "/login")
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	} else if err != nil {
-		// some other error
-		panic(err.Error())
-	} else {
-		// success - call the next handler
-		h.next.ServeHTTP(w, r)
-	}
-}
-
 // LoginHandler handles the third-party login process
 // format: /auth/{action}/{provider}
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
